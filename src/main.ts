23195d7f00f7f96e7cabad7a35fbed7e134eb8c4
huyhung27599 <huyhung27599@gmail.com>
2023-08-16 22:18:41 +0700

    config basic

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const { PORT } = process.env;
+  const { PORT, COOKIE_SECRET } = process.env;
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableCors({ origin: ['http://localhost:3000'], credentials: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.set('trust proxy', 'loopback');
 
   try {
     await app.listen(PORT, () => {
